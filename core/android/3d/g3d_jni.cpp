flyskywhy/react-native-gcanvas

commit 0c51446f803ee81e4fb7f2980d838268758b39c4

refactor: on Android, '2d' and  'webgl' will reset scale more quickly with the issue context was scaled when `Canvas resize` #32

core/android/3d/g3d_jni.cpp
@@ -69,22 +69,6 @@ Java_com_taobao_gcanvas_surface_GTextureViewCallback_onSurfaceChanged(JNIEnv *en
     if (!render) {
         LOG_D("onSurfaceChanged new render : %s", str_chars);
         render = GManager::getSingleton()->newRenderer(cxx_string);
-    } else {
-        if (render->m_width != width || render->m_height != height) {
-            float ratio = render->m_device_pixel_ratio;
-            int type = render->m_context_type;
-
-            LOG_D("onSurfaceChanged remove render : %s", str_chars);
-            GManager::getSingleton()->removeRenderer(cxx_string);
-            LOG_D("onSurfaceChanged new render : %s", str_chars);
-            render = GManager::getSingleton()->newRenderer(cxx_string);
-
-            render->m_device_pixel_ratio = ratio;
-            render->m_context_type = type;
-            LOG_D("request create canvas start");
-            render->requestCreateCanvas(cxx_string);
-            LOG_D("request create canvas end");
-        }
     }
 
     if (render) {

core/android/3d/view/grenderer.cpp
@@ -138,6 +138,12 @@ bool GRenderer::initialize() {
     return true;
 }
 
+void GRenderer::reInitialize() {
+    surfaceExit();
+    contextExit();
+    initialize();
+}
+
 void GRenderer::start() {
     m_requestExit = false;
     pthread_create(&m_thread_id, 0, threadStartCallback, this);
@@ -182,6 +188,10 @@ void GRenderer::destroy() {
         delete p;
     }
 
+    contextExit();
+}
+
+void GRenderer::contextExit() {
     LOG_D("context destroy in thread.");
 
     if (m_egl_context != EGL_NO_CONTEXT && m_egl_display != EGL_NO_DISPLAY) {
@@ -237,6 +247,14 @@ void GRenderer::renderLoop() {
                     break;
                 }
                 if (mProxy) mProxy->setContextLost(false);
+            } else {
+                // need this, otherwise:
+                // 2d: will need 2nd ctx draw something to display right canvas size
+                // webgl: previous 3d object will display on somewhere
+                reInitialize();
+
+                mProxy->ReCreateContext();
+                mProxy->SetContextType(m_context_type);
             }
 
             mProxy->OnSurfaceChanged(0, 0, m_width, m_height);

core/android/3d/view/grenderer.h
@@ -35,6 +35,8 @@ public:
 
     bool initialize();
 
+    void reInitialize();
+
     void signalUpGLthread();
 
     void setRefreshFlag(bool refresh);
@@ -53,6 +55,8 @@ public:
                        int format, int type);
 
     void surfaceDestroy();
+
+    void contextExit();
 
     void surfaceExit();
 

core/src/GCanvasWeex.cpp
@@ -51,6 +51,11 @@ void GCanvasWeex::CreateContext() {
     mCanvasContext->mContextId = this->mContextId;
 }
 
+void GCanvasWeex::ReCreateContext() {
+    delete mCanvasContext;
+    CreateContext();
+}
+
 void GCanvasWeex::Clear() {
     LOG_D("GCanvasWeex::DoContextLost start.");
     // No need to clean up GL memory with glDeleteBuffers or glDeleteTextures.

core/src/GCanvasWeex.hpp
@@ -126,6 +126,8 @@ public:
 
     virtual void CreateContext();
 
+    virtual void ReCreateContext();
+
     virtual void Clear();
 
 

packages/gcanvas/src/env/canvas.js
@@ -57,7 +57,7 @@ export default class GCanvas extends Element {
 
   set width(value) {
     this._width = value;
-    GCanvas.GBridge.callResetGlViewport(this.id, this._width, this._height);
+    GCanvas.GBridge.callResetGlViewport(this.id, this._width * PixelRatio.get(), this._height * PixelRatio.get());
   }
 
   get height() {
@@ -66,7 +66,7 @@ export default class GCanvas extends Element {
 
   set height(value) {
     this._height = value;
-    GCanvas.GBridge.callResetGlViewport(this.id, this._width, this._height);
+    GCanvas.GBridge.callResetGlViewport(this.id, this._width * PixelRatio.get(), this._height * PixelRatio.get());
   }
 
   getContext(type) {

commit 8abdcc1d0ead008a973ec854ab8bafa485c6c708

fix: (again 06a7064) a bug that will no display while quit from a drawer item page to current webgl page which is still maintain mounted by react-navigation on Android caused by 0c51446 in v2.3.10

core/android/3d/g3d_jni.cpp
@@ -70,9 +70,7 @@ Java_com_taobao_gcanvas_surface_GTextureViewCallback_onSurfaceChanged(JNIEnv *en
     if (!render) {
         LOG_D("onSurfaceChanged new render : %s", str_chars);
         render = GManager::getSingleton()->newRenderer(cxx_string);
-    }
-
-    if (render) {
+    } else if (render->m_width != width || render->m_height != height) {
         ANativeWindow *window = ANativeWindow_fromSurface(env, surface);
         LOG_D("onSurfaceChanged ANativeWindow_fromSurface");
         render->setNativeWindow(window);